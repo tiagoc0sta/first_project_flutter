08dab5f5b9068df19e722c0c9ef0a33e11980dfa
Tiago Costa <tiagodpv@gmail.com>
2023-09-21 21:29:52 -0400

    sec

---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,23 @@
+// Container(
+// color: Colors.white,
+// child: Column(children: [
+// Container(
+// color: Colors.red,
+// width: 100,
+// height: 100,
+// ),
+// Container(
+// color: Colors.blue,
+// width: 50,
+// height: 50,
+// ),
+// Center(
+// child: Container(
+// margin: const EdgeInsets.all(10.0),
+// color: Colors.amber[600],
+// width: 48.0,
+// height: 48.0,
+// ),
+// )
+// ]),
+// )
